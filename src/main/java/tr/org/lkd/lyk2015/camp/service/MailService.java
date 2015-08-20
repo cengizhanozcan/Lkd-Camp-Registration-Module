efe1401f62178a2a3cdbe9323856bf489ff16db4
Cengizhan Özcan <cengizhan_ozcan@hotmail.com>
2015-08-20 10:24:20 +0300

    version 1.0.11

--- src/main/java/tr/org/lkd/lyk2015/camp/service/MailService.java
+++
@@ -1,9 +0,0 @@
-package tr.org.lkd.lyk2015.camp.service;
-
-/*
-*cengizhan - Aug 19, 2015
-*/
-public interface MailService {
-
-	Boolean sendActivationEmail(String sender, String subject, String content);
-}
